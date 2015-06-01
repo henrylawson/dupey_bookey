9a02dade1addc5afc93582ba4d130076382707ff
Henry Lawson <henry.lawson@foinq.com>
2015-06-01 22:38:10 +0800

    Adding in more to readme, breaking 80 chars

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Intro
 Basic tool to find dupes in my booklist
+
+# Getting started
+To get started simply:
+1. bundle install
+1. bundel exec rspec
+1. bundle exec ./main.rb /path/to/books.json
+
+# Assumption
+The books json file is formatted similar to what is in the specs
--- main.rb
+++ main.rb
@@ -4,4 +4,6 @@ require './lib/dupey_bookey'
 
 books_file = ARGV[0] ||= 'spec/fixture/books.json'
 file = File.open(books_file)
-DupeyBookey.new.dupes(file.read).each_with_index { |book, index| puts "#{index+1}: #{book}" }
+DupeyBookey.new.dupes(file.read).each_with_index do |book, index|
+  puts "#{index+1}: #{book}"
+end
